79ff1f6098b6fb5706395d6fb3bfa2766d8205f2
Jeremy Andy Ampatin <jeremyandyampatin@gmail.com>
2024-08-21 15:35:38 +0800

    chore(entity): added product entity

---
+++ src/main/java/com/accenture/productinfo/dto/ProductIdResponse.java
@@ -0,0 +1,21 @@
+package com.accenture.productinfo.dto;
+
+public class ProductIdResponse {
+
+    private String productId;
+
+    public ProductIdResponse(){}
+
+    public ProductIdResponse(String productId){
+
+        this.productId = productId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
