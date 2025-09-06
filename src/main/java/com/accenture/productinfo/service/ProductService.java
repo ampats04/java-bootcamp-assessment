c1b702785b47c7153be4d05c1f40a25789f0ae81
Jeremy Andy F. Ampatin <106399501+ampats04@users.noreply.github.com>
2025-09-06 17:52:19 +0800

    Delete src/main/java/com/accenture/productinfo/service directory

--- src/main/java/com/accenture/productinfo/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.accenture.productinfo.service;
-
-import com.accenture.productinfo.dto.ProductIdResponse;
-
-public interface ProductService {
-
-    ProductIdResponse getProductId(String productCode);
-}
